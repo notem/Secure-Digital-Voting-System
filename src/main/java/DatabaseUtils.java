59c16ae54aaff519a3e58a390a8d9cb58672a740
notem <njm3308@rit.edu>
2018-03-08 13:59:18 -0500

    minor_feat: created an (incomplete) function which creates the database elements necessary for a new election blockchain

--- src/main/java/DatabaseUtils.java
+++ src/main/java/DatabaseUtils.java
@@ -126,4 +126,58 @@ public class DatabaseUtils
             return list;
         }
     }
+
+    /**
+     * creates a new table to hold an elections blockchain
+     * adds an entry for that election blockchain to the elections table (for easier referencing)
+     * inserts the genesis block (contains blockchain encryption key)
+     * @param publicKey the encryption key (RSA-4096) for ballots urlbase64 encoded
+     * @return true is election creation was successful
+     */
+    public static Boolean initializeElectionBlockchain(String publicKey)
+    {
+        if (connection == null) return false;
+        String rst; PreparedStatement pst;
+        try
+        {
+            // create an elections table to hold meta-information
+            // on existing election block chains
+            rst = "CREATE TABLE IF NOT EXISTS elections (" +
+                    "public_key VARCHAR(683) PRIMARY KEY," +
+                    "block_count BIGINT NOT NULL" +     // next block number
+                    // TODO other useful information to keep handy?
+                    ");";
+            connection.prepareStatement(rst).executeUpdate();
+
+            // create the new election block chain
+            rst = "CREATE TABLE e"+publicKey+" (" +
+                    "_id BIGSERIAL PRIMARY KEY, " +            // arbitrary, unique ID
+                    "block_no BIGINT NOT NULL, " +             // block number
+                    "block_content VARCHAR(638) NOT NULL, " +  // contents of the block -> urlbase64 encoded
+                    "_time BIGINT NOT NULL," +                 // epoch time
+                    "current_hash VARCHAR(43) NOT NULL" +      // hash(content||prev_hash||time) -> urlbase64 encoded
+                    ");";
+            connection.prepareStatement(rst).executeUpdate();
+
+            // insert the genesis block into the table
+            rst = "INSERT INTO e"+publicKey+" VALUES(NULL, ?, ?, '', ?);";
+            pst = connection.prepareStatement(rst);
+            pst.setInt(1, 0);
+            pst.setString(2, publicKey);
+            pst.setString(3, "[signature]");   // TODO create function to generate hash and sign with public key
+
+            // insert an entry into the table of elections
+            rst = "INSERT INTO elections VALUES(?,?);";
+            pst = connection.prepareStatement(rst);
+            pst.setString(1, publicKey);
+            pst.setInt(2, 1);
+
+            return 1 == pst.executeUpdate(); // return true if the entry was created
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
